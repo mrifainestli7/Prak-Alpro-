49478764d0594acf5e1b16525198114644a54cb5
mrifainestli7 <101726009+mrifainestli7@users.noreply.github.com>
2022-04-07 10:03:29 +0700

    Update output.h

--- Pratikum 3/studi kasus/Kolaborasi3/library/output.h
+++ Pratikum 3/studi kasus/Kolaborasi3/library/output.h
@@ -3,18 +3,18 @@ using namespace std;
 class Output {
 	public :
 		void cetak(){
-            system("CLS");
-			cout << "STRUK PEMBELIAN MAKANAN \n";
-			cout << "Yang dibeli : \n";
-			cout << "  Ayam Geprek  -> " << data_file[4] << endl;
-			cout << "  Ayam goreng  -> " << data_file[5] << endl;
-            cout << "  Udang goreng -> " << data_file[6] << endl;
-            cout << "  Cumi goreng  -> " << data_file[7] << endl;
-            cout << "  Ayam bakar   -> " << data_file[8] << endl;
-			cout << "Harga total Rp. " << data_file[0] << endl;
-            cout << "Ongkir      Rp. " << data_file[1] << endl;
-			cout << "Diskon      Rp. " << data_file[2] << endl;
-			cout << "Harga Bayar Rp. " << data_file[3] << endl;
+                system("CLS");
+		cout << "STRUK PEMBELIAN MAKANAN \n";
+		cout << "Yang dibeli : \n";
+		cout << "  Ayam Geprek  -> " << data_file[4] << endl;
+		cout << "  Ayam goreng  -> " << data_file[5] << endl;
+                cout << "  Udang goreng -> " << data_file[6] << endl;
+                cout << "  Cumi goreng  -> " << data_file[7] << endl;
+                cout << "  Ayam bakar   -> " << data_file[8] << endl;
+		cout << "Harga total Rp. " << data_file[0] << endl;
+                cout << "Ongkir      Rp. " << data_file[1] << endl;
+		cout << "Diskon      Rp. " << data_file[2] << endl;
+		cout << "Harga Bayar Rp. " << data_file[3] << endl;
 		}
 
 		void getData(){
